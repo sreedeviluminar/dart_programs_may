f6e10881291dd071832a0e487d92c71e075c0cd9
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-05-18 12:22:00 +0530

    commit

--- bin/operators.dart
+++ bin/operators.dart
@@ -29,7 +29,7 @@ void main(){
   print('c == d  -> ${c == d}');
   print('c != d  -> ${c != d}');
 
-  ///4. conditional operator  &&  ||  !
+  ///4. Logical operator  &&  ||  !
 
   print("-----------------------");
   String username = "admin";
@@ -39,4 +39,35 @@ void main(){
   print(  username == "abc" ||  password == 'abc123');  // true
   print(!(username == "abc" &&  password == 'abc123')); // !false = true  (opposite of actual result)
 
+  ///5.Unary operator  postfix prefix
+  ///postfix -   variable++    variable--  eg: int a= 10   a++  a--
+  ///prefix  -   ++variable    --variable  eg: int b= 20   ++b  --b
+
+  print("-----------------------");
+  int x = 100;
+  print('x++ = ${x++}');//print x=100  then increment value of x  x = x+1 = 100 + 1= 101 this will be stored for next step
+  print(x);
+  print('x-- = ${x--}');// x = 101 background x = x- 1 = 101 - 1 = 100
+  print('++x = ${++x}');// x = x + 1 = 100 + 1= 101
+  print('--x = ${--x}');// x = x - 1 = 101 - 1= 100
+  print(x++); // x = 100  background  x = 101 will be stored  (x+1 = 100+1 = 101 )
+
+  ///6. typetest operator   is    is!
+  var name = "AlAN";
+  print(name is! String);
+  print(name is int);
+
+
+  ///7. bitwise and shift operator- to perform operations in binary values (0's and 1's)
+
+  ///8. Conditional/ ternary operator   syntax : condition ? true statement : false statement;
+  int age = 20;
+  var result = age >= 18 ? "Eligible to vote" : false;
+  print(result);
+
+
+  String uname = "admin";
+  String pass  = "admin1";
+  var out = uname =='admin' && pass == '123' ? "Login Success" : 'Login Failed';
+  print(out);
 }
